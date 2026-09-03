4f8f4934a238987fb0f4eb4195ea3f3b535751f2
IzenAbbas <izen.khokhar@gmail.com>
2026-09-03 16:41:56 +0500

    feat(WeSplit): add project files and update ContentView

---
+++ SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ali Abbas on 03/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, World")
+                Text("Hello, World")
+                Text("Hello, World")
+                Text("Hello, World")
+                Text("Hello, World")
+            }
+            Section {
+                Text("Hello, World")
+                Text("Hello, World")
+            }
+            Text("Hello, World")
+            Text("Hello, World")
+            Text("Hello, World")
+            Text("Hello, World")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ali Abbas on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
